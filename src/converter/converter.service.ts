64737e826e75a27278f6a61fc65193ae59ac34ca
Israel <isrovick@gmail.com>
2023-12-20 22:48:17 -0400

    Feat: 🦾 Converter service funtionality
    - Added Converter functionality to converterService

--- src/converter/converter.service.ts
+++ src/converter/converter.service.ts
@@ -1,10 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { SingleExpectationType } from '../types/converter.type';
 
 @Injectable()
 export class ConverterService {
   constructor() {}
   convert(source: string): Array<SingleExpectationType> {
-    return;
+    let _source;
+    try {
+      _source = JSON.parse(source);
+    } catch (e) {
+      throw new BadRequestException('Invalid JSON');
+    }
+
+    const records = _source?.Records;
+    if (!records?.length) {
+      throw new BadRequestException('Invalid JSON, Missing records object.');
+    }
+
+    return records.map((record: any) => this.convertSingleRecord(record));
+  }
+
+  convertSingleRecord(record: any): SingleExpectationType {
+    if (!record) {
+      return {} as SingleExpectationType;
+    }
+
+    const receipt = record?.ses?.receipt;
+    const mail = receipt?.mail;
+
+    const spam = receipt?.spamVerdict?.status === 'PASS';
+
+    const virus = receipt?.virusVerdict?.status === 'PASS';
+
+    const dns =
+      receipt?.spfVerdict?.status === 'PASS' &&
+      receipt?.dkimVerdict?.status === 'PASS' &&
+      receipt?.dmarcVerdict?.status === 'PASS';
+
+    const mes = new Date(mail?.timestamp).toLocaleString('es-ES', {
+      month: 'long',
+    });
+
+    const retrasado = receipt?.processingTimeMillis > 1000;
+
+    const emisor = mail?.source?.replace(/@.*/, '');
+
+    const receptor =
+      mail?.destination?.map((d: string) => d.replace(/@.*/, '')) || [];
+
+    return {
+      spam,
+      virus,
+      dns,
+      mes,
+      retrasado,
+      emisor,
+      receptor,
+    };
   }
 }
